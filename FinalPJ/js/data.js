76fbebd1b82be4427096a0ef3b1e57d363702e5f
Nguyen Khanh <148553023+kerbthecurb@users.noreply.github.com>
2024-01-31 17:14:26 +0700

    Add files via upload

--- FinalPJ/js/data.js
+++ FinalPJ/js/data.js
@@ -4,7 +4,7 @@ const paintings = [
       artist: "Claude Monet",
       year: 1886,
       description: "A beautiful depiction of cherry blossoms in full bloom along the Seine River in Paris.",
-      image: "https://example.com/springtime-in-paris.jpg",
+      image: "",
     },
     {
       title: "Primavera",
@@ -12,21 +12,9 @@ const paintings = [
       year: 1482,
       description: "An allegorical painting representing the arrival of spring with a group of mythological figures.",
       image: "https://example.com/primavera.jpg",
-    },
-    {
-      title: "The Hay Wain",
-      artist: "John Constable",
-      year: 1821,
-      description: "A landscape painting showing a hay wain (a type of horse-drawn cart) crossing a river in the English countryside during springtime.",
-      image: "https://example.com/the-hay-wain.jpg",
-    },
-    {
-      title: "Spring",
-      artist: "Édouard Manet",
-      year: 1881,
-      description: "A portrait of a young woman holding a bunch of spring flowers, symbolizing the arrival of the season.",
-      image: "https://example.com/spring.jpg",
     }
+    
   ];
   
   console.log(paintings);
+  localStorage.setItem("paintings", JSON.stringify(paintings));
